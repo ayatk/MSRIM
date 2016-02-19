04a24213e5fecca6e63e199fe4a92b026306b07a
Aya Shigure <ayatk@users.noreply.github.com>
2016-02-20 02:46:56 +0900

    jar_test.goを追加

---
+++ jar_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestUnJar(t *testing.T) {
+	jarSrc := "test_assets/test.jar"
+	jarDest := "test_assets/dest"
+
+	if err := UnJar(jarSrc, jarDest); err != nil {
+		t.Fatalf("Un zipping error: %v", err)
+	}
+	os.RemoveAll(jarDest)
+}
