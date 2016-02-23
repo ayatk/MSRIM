9f952735fdddaf4571fc1f4268e9e5a0ddad7c35
Aya Shigure <ayatk@users.noreply.github.com>
2016-02-23 15:09:46 +0900

    Add StripBOM

--- utils/util.go
+++ utils/util.go
@@ -1,9 +1,19 @@
 package utils
 
-import "os"
+import (
+	"bytes"
+	"os"
+)
+
+var utf8bom = []byte{239, 187, 191}
 
 // Exists is return true if the file exists
 func Exists(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
+}
+
+// utf8のbomを消す
+func StripBOM(in []byte) []byte {
+	return bytes.TrimPrefix(in, utf8bom)
 }
--- utils/util_test.go
+++ utils/util_test.go
@@ -1,27 +1,53 @@
 package utils
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type existsTest struct {
 	path    string
 	boolean bool
 }
 
-var existsTests = []existsTest{
-	{"../main.go", true},
-	{"../script/compile", true},
-	{"../test_assets/test.jar", true},
-	{"../unknown-dir", false},
-	{"../unknown-file.txt", false},
-	{"../unknown-dir/unknown-file.txt", false},
-}
-
 func TestExists(t *testing.T) {
+	var existsTests = []existsTest{
+		{"../main.go", true},
+		{"../script/compile", true},
+		{"../test_assets/test.jar", true},
+		{"../unknown-dir", false},
+		{"../unknown-file.txt", false},
+		{"../unknown-dir/unknown-file.txt", false},
+	}
+
 	for i := range existsTests {
 		test := &existsTests[i]
 		if Exists(test.path) != test.boolean {
-			t.Errorf("Failed chack exists: %v => %t wants %t",
-				test.path, Exists(test.path), test.boolean)
+			t.Errorf("Failed chack exists: %v => %t wants %t", test.path, Exists(test.path), test.boolean)
+		}
+	}
+}
+
+func TestStripBOM(t *testing.T) {
+	var bom = [][]byte{
+		{239, 187, 191, 244, 145, 128, 216, 130, 239, 116},
+		{105, 155, 243, 209, 202, 129, 156, 114, 236, 138},
+		{149, 183, 173, 138, 182, 134, 197, 127, 120, 100},
+		{239, 187, 191, 255, 194, 159, 105, 195, 241, 167},
+	}
+
+	var answer = [][]byte{
+		{244, 145, 128, 216, 130, 239, 116},
+		{105, 155, 243, 209, 202, 129, 156, 114, 236, 138},
+		{149, 183, 173, 138, 182, 134, 197, 127, 120, 100},
+		{255, 194, 159, 105, 195, 241, 167},
+	}
+
+	for i := range bom {
+		st := bom[i]
+		ans := answer[i]
+		if !reflect.DeepEqual(StripBOM(st), ans) {
+			t.Errorf("Failed remove BOM")
 		}
 	}
 }
